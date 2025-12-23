2d7fa4bfa8a30e56686e8592e1ded10863bc6bcc
dekhodun <dekhodun@gmail.com>
2025-12-23 22:27:50 +0530

    component completed

---
+++ 02_Components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+
+const user = "Raj Kishor"
+
+  return (
+    <div>
+      <h1>hello I'm {user} </h1>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 02_Components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+       <div className='card' >
+        <h1>RAJ KISHOR</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis culpa, blanditiis praesentium obcaecati corporis.</p>
+       </div>
+    </>
+  )
+}
+
+export default Card
