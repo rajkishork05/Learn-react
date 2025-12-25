7e20003209e802bc031bef215a1eb1a947374dd0
dekhodun <dekhodun@gmail.com>
2025-12-25 21:26:43 +0530

    tailwind initialize

---
+++ 05_tailwindcss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center min-h-[100vh]' >
+      <button className='w-30 h-15 text-2xl font-extrabold text-emerald-50 rounded border-6 active:scale-90 hover:scale-110 transition-all border-amber-500 bg-blue-600' >Click me</button>
+    </div>
+  )
+}
+
+export default App
