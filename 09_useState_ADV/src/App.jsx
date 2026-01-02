832b2ea737624ed3d3d33d85900d47fd9b4f606e
dekhodun <kothiyalrajkishor1@gmail.com>
2026-01-02 22:48:23 +0530

    batch update

--- 09_useState_ADV/src/App.jsx
+++ 09_useState_ADV/src/App.jsx
@@ -2,17 +2,44 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [num, SetNum] = useState(0)
+  // const [num, SetNum] = useState(0)
+  // const [obj, SetObj] = useState({user: "raj", age:31})
+  const [arr, SetArr] = useState(["adi", "rose", "marathi", "lily"])
 
-  const change = async()=>{
-    await SetNum(num+1)
-    await console.log(num);
+
+  const change = ()=>{
+    // SetNum(num+1)
+    // console.log(num);
+
+    //destructuring objects
+    // const newobj = {...obj};
+    // newobj.user = "kapru";
+    // newobj.age = 50
+    // console.log(newobj.user, newobj.age)
+    // SetObj(newobj)
+    // console.log(obj.user, obj.age)
+
+    //Array destructuring
+    const newArr = [...arr];
+    newArr[2] = "jash";
+    // console.log(newArr)
+    // SetArr(newArr)
+
+    //direct method
+    SetArr(prev => ([...prev, "jasmine",] ))
+
+
   }
 
   return (
     <div>
-      <div className=' bg-cyan-800 h-screen w-screen flex justify-center items-center flex-col gap-10 ' >
-        <h1 className='bg-cyan-500 text-5xl py-3 px-5 rounded-3xl ' >{num}</h1>
+      <div className=' bg-cyan-800 h-screen w-screen flex justify-center items-center  gap-10 ' >
+        {/* <h1 className='bg-cyan-500 text-5xl py-3 px-5 rounded-3xl ' >{num}</h1> */}
+        {/* <h1 className='bg-cyan-500 text-5xl py-3 px-5 rounded-3xl ' >{obj.user} {obj.age}</h1> */}
+        {arr.map(function(ele, idx){
+          return <h1 key={idx} className='bg-cyan-500 text-5xl py-3 px-5 rounded-3xl ' >{ele} </h1>
+
+        })}
         <button onClick={change} className='text-5xl font-semibold bg-cyan-400 py-4 px-7 rounded-3xl active:scale-95 cursor-pointer' >click me</button>
       </div>
     </div>
