d3b4b134dbe6cc30c6957498342811e85f24d9aa
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-28 14:26:45 +0530

    work on useRef

--- 22_useRef/src/App.jsx
+++ 22_useRef/src/App.jsx
@@ -3,6 +3,9 @@ import Focus from './component/Focus'
 import Input_val from './component/Input_val'
 import Prev_val from './component/Prev_val'
 import Runs_for_once from './component/Runs_for_once'
+import Times_render from './component/Times_render'
+import Message_scroll from './component/Message_scroll'
+import Click_Detect from './component/Click_Detect'
 
 const App = () => {
   return (
@@ -10,7 +13,10 @@ const App = () => {
       {/* <Focus/> */}
       {/* <Input_val/> */}
       {/* <Prev_val/> */}
-      <Runs_for_once/>
+      {/* <Runs_for_once/> */}
+      {/* <Times_render/> */}
+      {/* <Message_scroll/> */}
+      <Click_Detect/>
     </div>
   )
 }
